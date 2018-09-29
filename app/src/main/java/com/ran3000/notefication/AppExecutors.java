af2b3c3c76591b4ff961439689e69b52f300e95a
Giacomo Randazzo <giacomoran@gmail.com>
2018-09-29 17:21:03 +0200

    add app executors

---
+++ app/src/main/java/com/ran3000/notefication/AppExecutors.java
@@ -0,0 +1,52 @@
+package com.ran3000.notefication;
+
+import android.os.Handler;
+import android.os.Looper;
+import android.support.annotation.NonNull;
+
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
+
+/**
+ * Global executor pools for the whole application.
+ *
+ * Groping tasks like this avoids the effects af task starvation (e.g. disk reads don't wait behind
+ * webservice requests).
+ *
+ * Taken from google android architecture samples:
+ * @see <a href="https://github.com/googlesamples/android-architecture/blob/todo-mvp/todoapp/app/src/main/java/com/example/android/architecture/blueprints/todoapp/util/AppExecutors.java">google android architecture samples</a>
+ */
+public class AppExecutors {
+
+    private static final int THREAD_COUNT = 3;
+
+    private final Executor diskIO;
+    private final Executor mainThread;
+
+    public AppExecutors(Executor diskIO, Executor mainThread) {
+        this.diskIO = diskIO;
+        this.mainThread = mainThread;
+    }
+
+    public AppExecutors() {
+        this(Executors.newSingleThreadExecutor(), new MainThreadExecutor());
+    }
+
+    public Executor diskIO() {
+        return diskIO;
+    }
+
+    public Executor mainThread() {
+        return mainThread;
+    }
+
+    private static class MainThreadExecutor implements Executor {
+        private Handler mainThreadHandler = new Handler(Looper.getMainLooper());
+
+        @Override
+        public void execute(@NonNull Runnable command) {
+            mainThreadHandler.post(command);
+        }
+    }
+
+}
