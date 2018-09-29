db1a86cd0394f5f09423362e6d49a1cac8b1dd3a
Giacomo Randazzo <giacomoran@gmail.com>
2018-09-29 15:38:06 +0200

    add Timber and Butterknife

--- app/src/main/java/com/ran3000/notefication/MainActivity.java
+++ app/src/main/java/com/ran3000/notefication/MainActivity.java
@@ -1,18 +1,35 @@
 package com.ran3000.notefication;
 
+import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Window;
 
+import butterknife.BindView;
+import butterknife.OnClick;
+
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.main_layout)
+    ConstraintLayout mainLayout;
+
+    private ColorManager colorManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        colorManager = new ColorManager();
+
         // no title bar
         this.requestWindowFeature(Window.FEATURE_NO_TITLE);
 
         setContentView(R.layout.activity_main);
+    }
+
+    @OnClick(R.id.main_layout)
+    public void changeColor() {
+        colorManager.nextColor();
+        mainLayout.setBackgroundColor(colorManager.getCurrentColor());
     }
 }
