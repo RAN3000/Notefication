16535897c876aeca0f3d3ff30e1735379e335f8b
Giacomo Randazzo <giacomoran@gmail.com>
2018-09-29 16:54:27 +0200

    add EditText

--- app/src/main/java/com/ran3000/notefication/MainActivity.java
+++ app/src/main/java/com/ran3000/notefication/MainActivity.java
@@ -5,9 +5,14 @@ import android.support.v4.content.ContextCompat;
 import android.support.v4.view.ViewCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.Window;
 import android.view.WindowManager;
+import android.view.inputmethod.EditorInfo;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -20,6 +25,10 @@ public class MainActivity extends AppCompatActivity {
     ConstraintLayout mainLayout;
     @BindView(R.id.main_background)
     Button mainBackground;
+    @BindView(R.id.main_edittext)
+    EditText mainEditText;
+    @BindView(R.id.main_send_button)
+    ImageButton mainButtonSend;
 
     private ColorManager colorManager;
 
@@ -43,6 +52,26 @@ public class MainActivity extends AppCompatActivity {
         Timber.d("Activity created.");
 
         ViewCompat.setTranslationZ(mainBackground, 1);
+        ViewCompat.setTranslationZ(mainEditText, 20);
+        ViewCompat.setTranslationZ(mainButtonSend, 20);
+
+
+        // edit text action send
+        mainEditText.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                if (actionId == EditorInfo.IME_ACTION_SEND) {
+                    sendNote();
+                    return true;
+                }
+                return false;
+            }
+        });
+    }
+
+    @OnClick(R.id.main_send_button)
+    public void sendNote() {
+        Timber.d("Note sent: %s", mainEditText.getText());
     }
 
     @OnClick(R.id.main_background)
