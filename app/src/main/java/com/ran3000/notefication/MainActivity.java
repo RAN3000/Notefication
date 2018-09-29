4825e638aa28122729115f8aba722e0e67bfa836
Giacomo Randazzo <giacomoran@gmail.com>
2018-09-29 15:34:11 +0200

    text and background

--- app/src/main/java/com/ran3000/notefication/MainActivity.java
+++ app/src/main/java/com/ran3000/notefication/MainActivity.java
@@ -2,12 +2,17 @@ package com.ran3000.notefication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Window;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        // no title bar
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+
         setContentView(R.layout.activity_main);
     }
 }
