8ca30ea434048826b3bc1624d2ed36ca615221e9
Giacomo Randazzo <giacomoran@gmail.com>
2018-09-29 15:19:10 +0200

    initial commit

---
+++ app/src/main/java/com/ran3000/notefication/MainActivity.java
@@ -0,0 +1,13 @@
+package com.ran3000.notefication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
