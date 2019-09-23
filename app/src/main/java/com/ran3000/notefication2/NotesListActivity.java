7b858f4f4995198c366bade4946098f9c150888c
Giacomo Randazzo <giacomoran@gmail.com>
2019-09-23 09:43:06 +0200

    Show list of notes in menu

--- app/src/main/java/com/ran3000/notefication2/NotesListActivity.java
+++ app/src/main/java/com/ran3000/notefication2/NotesListActivity.java
@@ -1,22 +1,43 @@
 package com.ran3000.notefication2;
 
 import android.app.Activity;
-import android.graphics.Color;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.core.content.ContextCompat;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import com.ran3000.notefication2.data.Note;
+import com.ran3000.notefication2.data.NoteDatabase;
+
+import java.util.List;
+
+import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 
 public class NotesListActivity extends Activity {
 
+    @BindView(R.id.notes_list_recycler_view)
+    RecyclerView recyclerView;
+    @BindView(R.id.notes_list_no_notes_text)
+    TextView noNotesTextView;
+
+    private AppExecutors executors;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        executors = new AppExecutors();
 
         int darkColor = getIntent().getIntExtra("darkColor", ColorManager.DARK_GREEN);
 
@@ -30,10 +51,78 @@ public class NotesListActivity extends Activity {
 
         setContentView(R.layout.activity_notes_list);
         ButterKnife.bind(this);
+
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        executors.diskIO().execute(() -> {
+            NoteDatabase database = NoteDatabase.getAppDatabase(this);
+
+            List<Note> notes = database.noteDao().getAll();
+
+            executors.mainThread().execute(() -> {
+                if (notes.size() > 0) {
+                    recyclerView.setAdapter(new NotesAdapter(notes));
+                } else {
+                    recyclerView.setVisibility(View.GONE);
+                    noNotesTextView.setVisibility(View.VISIBLE);
+                }
+            });
+        });
     }
 
     @OnClick(R.id.notes_list_up_button)
     public void goToScreenUp() {
         onBackPressed();
     }
+
+
+    class NotesAdapter extends RecyclerView.Adapter<NotesAdapter.NoteViewHolder> {
+        private List<Note> notes;
+
+        class NoteViewHolder extends RecyclerView.ViewHolder {
+            // each data item is just a string in this case
+            TextView textView;
+            RelativeLayout layout;
+            NoteViewHolder(View v) {
+                super(v);
+                textView = v.findViewById(R.id.notification_text);
+                layout = v.findViewById(R.id.notification_layout);
+            }
+        }
+
+        NotesAdapter(List<Note> notes) {
+            this.notes = notes;
+        }
+
+        @NonNull
+        @Override
+        public NotesAdapter.NoteViewHolder onCreateViewHolder(@NonNull ViewGroup parent,
+                                                              int viewType) {
+            // create a new view
+            View v = LayoutInflater.from(parent.getContext())
+                    .inflate(R.layout.notification_expanded_layout, parent, false);
+            return new NoteViewHolder(v);
+        }
+
+        @Override
+        public void onBindViewHolder(@NonNull NoteViewHolder holder, int position) {
+            Note note = notes.get(position);
+            holder.textView.setText(note.getText());
+            holder.layout.setBackgroundResource(note.getColor());
+        }
+
+        @Override
+        public int getItemCount() {
+            return notes.size();
+        }
+    }
+
 }
+
+
